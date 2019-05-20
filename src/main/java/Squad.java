42d3535e3d85f509c3460d70187537657175b350
shariff6 <shariff.mohammed.a@gmail.com>
2019-05-20 11:17:04 +0300

    creates squad class, encapsulates it, and adds apprproate methods to the class.

---
+++ src/main/java/Squad.java
@@ -0,0 +1,49 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Squad {
+    private String mName;
+    private Integer mMaxSize;
+    private static ArrayList<Squad> instances = new ArrayList<Squad>();
+    private int mId;
+    private List<Hero> mHeros;
+
+    public Squad(String name) {
+        mName = name;
+        instances.add(this);
+        mId = instances.size();
+        mHeros = new ArrayList<Hero>();
+    }
+
+    public String getName() {
+        return mName;
+    }
+    public Integer getMaxSize() {
+        return mMaxSize;
+    }
+
+    public static ArrayList<Squad> all() {
+        return instances;
+    }
+
+    public static void clear() {
+        instances.clear();
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public static Squad find(int id) {
+        return instances.get(id - 1);
+    }
+
+    public List<Hero> getHeros() {
+        return mHeros;
+    }
+
+    public void addHero(Hero hero) {
+        mHeros.add(hero);
+    }
+
+}
