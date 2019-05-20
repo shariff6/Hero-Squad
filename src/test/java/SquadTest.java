17d1e8d989fc50b892fa3d74212f93da4c41612b
shariff6 <shariff.mohammed.a@gmail.com>
2019-05-20 14:34:39 +0300

    ensures all tests pasts by adding correct attributes to squad

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -5,27 +5,27 @@ public class SquadTest {
 
     @Test
     public void squad_instantiatesCorrectly_true() {
-        Squad testSquad = new Squad("Marvel");
+        Squad testSquad = new Squad("Marvel" , 1 );
         assertEquals(true, testSquad instanceof Squad);
     }
 
     @Test
     public void getName_squadInstantiatesWithName_Marvel() {
-        Squad testSquad = new Squad("Marvel");
+        Squad testSquad = new Squad("Marvel" , 1 );
         assertEquals("Marvel", testSquad.getName());
     }
 
     @Test
     public void all_returnsAllInstancesOfSquad_true() {
-        Squad firstSquad = new Squad("Marvel");
-        Squad secondSquad = new Squad("Dc");
+        Squad firstSquad = new Squad("Marvel" , 1 );
+        Squad secondSquad = new Squad("Dc", 5);
         assertEquals(true, Squad.all().contains(firstSquad));
         assertEquals(true, Squad.all().contains(secondSquad));
     }
 
     @Test
     public void clear_emptiesAllSquadsFromList_0() {
-        Squad testSquad = new Squad("Marvel");
+        Squad testSquad = new Squad("Marvel" , 1 );
         Squad.clear();
         assertEquals(Squad.all().size(), 0);
     }
@@ -33,29 +33,29 @@ public class SquadTest {
     @Test
     public void getId_squadsInstantiateWithAnId_1() {
         Squad.clear();
-        Squad testSquad = new Squad("Marvel");
+        Squad testSquad = new Squad("Marvel" , 1 );
         assertEquals(1, testSquad.getId());
     }
 
     @Test
     public void find_returnsSquadWithSameId_secondSquad() {
         Squad.clear();
-        Squad firstSquad = new Squad("Marvel");
-        Squad secondSquad = new Squad("Dc");
+        Squad firstSquad = new Squad("Marvel" , 1 );
+        Squad secondSquad = new Squad("Dc", 5);
         assertEquals(Squad.find(secondSquad.getId()), secondSquad);
     }
 
     @Test
     public void getHeros_initiallyReturnsEmptyList_ArrayList() {
         Squad.clear();
-        Squad testSquad = new Squad("Marvel");
+        Squad testSquad = new Squad("Marvel" , 1 );
         assertEquals(0, testSquad.getHeros().size());
     }
 
     @Test
     public void addHero_addsHeroToList_true() {
-        Squad testSquad = new Squad("Marvel");
-        Hero testHero = new Hero("Mow the lawn");
+        Squad testSquad = new Squad("Marvel" , 1 );
+        Hero testHero = new Hero("Superman", 1, "SuperStrength", "Kyrypton");
         testSquad.addHero(testHero);
         assertTrue(testSquad.getHeros().contains(testHero));
     }
